ffc74583ca0e3a85e353ecd25153d276a65fefa0
Donkemezuo <raymonddonkemezuo.org>
2019-02-19 15:01:22 -0500

    Work done

--- 9squareApp/Controllers/DetailViewController.swift
+++ 9squareApp/Controllers/DetailViewController.swift
@@ -12,6 +12,8 @@ class DetailViewController: UIViewController {
     
     let detailView = DetailView()
     private var restuarant:VenueStruct!
+    var tabBarButton = UIBarButtonItem()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,8 @@ class DetailViewController: UIViewController {
         view.backgroundColor = .red
         detailView.venueName.text = restuarant.name
         detailView.venueDescription.text = restuarant.location.modifiedAddress
+        addVenue()
+        
         
         if let linkExists = restuarant.imageLink {
             if let imageIsInCache = ImageHelper.fetchImageFromCache(urlString: linkExists) {
@@ -29,11 +33,62 @@ class DetailViewController: UIViewController {
                         print("imageHelper in detail vc error = \(appError)")
                     } else if let image = image {
                         self.detailView.venueImage.image = image
+
+                        
                     }
                 }
             }
         }
     }
+    
+    
+    private func addVenue(){
+        tabBarButton =  UIBarButtonItem.init(barButtonSystemItem: .add, target: self, action: #selector(addToCollection))
+        navigationItem.rightBarButtonItem = tabBarButton
+    }
+    
+    @objc private func addToCollection(){
+        
+    let alertController =  UIAlertController(title: "", message: "Please enter a category name you want to save this venue", preferredStyle: .alert)
+        
+        let save = UIAlertAction(title: "Submit", style: .default) { (alert) in
+            //this is when they press submit
+            let savingDate = Date.getISOTimestamp()
+            guard let collectionName = alertController.textFields?.first?.text else {return}
+            if let imageData = self.detailView.venueImage.image {
+                let favoritedVenueImage = imageData.jpegData(compressionQuality: 0.5)
+                let venueToSave = FaveRestaurant.init(collectionName: collectionName, restaurantName: self.restuarant.name, favoritedAt: savingDate, imageData: favoritedVenueImage, description: (self.restuarant.categories.first?.name)!, venue: self.restuarant.location.modifiedAddress)
+                
+                RestaurantDataManager.saveToDocumentDirectory(newFavoriteRestaurant: venueToSave)
+                
+            
+//                self.present(UIAlertController.init(title: "test", message: "soketing", preferredStyle: .alert ), animated: true, completion: nil)
+//
+         self.showAlert(title: "Success", message: "Successfully saved venue to \(collectionName)")
+//                print("Successfully saved venue to \(collectionName)")
+            }
+            
+        }
+        
+        let cancel = UIAlertAction.init(title: "Cancel", style: .cancel, handler: { (alert) in
+            
+        })
+        
+
+        alertController.addTextField {(textfield) in
+            textfield.placeholder = "Enter collection name"
+            textfield.textAlignment =  .center
+            
+        }
+        
+        alertController.addAction(save)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true)
+        
+    }
+    
+    
     
     init(restuarant: VenueStruct){
         super.init(nibName: nil, bundle: nil)
