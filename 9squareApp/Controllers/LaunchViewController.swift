fc9832e346085ef1f5402db5cd09f571d15e5454
GMosquera1 <genesismosquera@pursuit.org>
2019-02-21 13:07:43 -0500

    created a new VC

---
+++ 9squareApp/Controllers/LaunchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LaunchViewController.swift
+//  9squareApp
+//
+//  Created by Genesis Mosquera on 2/21/19.
+//  Copyright © 2019 EnProTech Group. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white 
+     
+    }
+    
+
+
+}
